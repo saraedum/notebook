4d16c73dfc5d8b05b93e4e97fb68c57308253f91
Carol Willing <carolcode@willingconsulting.com>
2016-03-05 18:31:56 -0800

    Update settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ import os
 import shlex
 
 # Needed for conversion from markdown to html
-import recommonmark.parser
+#import recommonmark.parser
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -58,15 +58,16 @@ extensions = [
     'sphinx.ext.napoleon',
     'IPython.sphinxext.ipython_console_highlighting',
     'sphinxcontrib.spelling',
+    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # Jupyter uses recommonmark's parser to convert markdown
-source_parsers = {
-    '.md': 'recommonmark.parser.CommonMarkParser',
-}
+# source_parsers = {
+#     '.md': 'recommonmark.parser.CommonMarkParser',
+# }
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
